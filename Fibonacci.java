63efb3e90cbf3add191eed3cd655f7fa5e55040f
Adam Home <adamrayner@live.com>
2016-06-24 20:24:33 +0200

    Added in functionality to discover the nth Fibonacci number using the command line

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,6 +2,31 @@ public class Fibonacci
 {
 	public static void main(String args[])
 	{
-		
+		System.out.println("The number " + args[0] + " Fibonacci number is " + fib(Integer.parseInt(args[0])) + ".");
+	}
+
+	public static int fib(int slot)
+	{
+		if(slot <= 0)
+		{
+			System.out.println("Please use a positive integer greater than or equal to 1.");
+			return -1;
+		}
+
+		else if(slot == 1)
+		{
+			return 0;
+		}
+
+		else if(slot == 2 || slot == 3)
+		{
+			return 1;
+		}
+
+		else
+		{
+			return (fib(slot -1) + fib(slot -2));
+		}
+
 	}
 }
